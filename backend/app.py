84c3d31ac310a84efe8ab7e982cede132a5326d8
Andrew Orals <nd-0r@andreworals.com>
2021-01-16 14:39:18 -0600

    started flask app.py

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+import flask
+from flask import json, Response
+from flask_cors import CORS
+from flask_api import FlaskAPI
+APP = FlaskAPI(__name__)
+CORS(APP)
+
+
+
+if __name__ == '__main__':
+  APP.run(host='127.0.0.1', port=9000)
