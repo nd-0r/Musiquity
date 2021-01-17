264752262f91a8196188c701d9d977f2baa16d59
Andrew Orals <nd-0r@andreworals.com>
2021-01-17 16:38:28 -0600

    preparing to make flask web api

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,28 @@ from search import Search
 APP = FlaskAPI(__name__)
 CORS(APP)
 
-
+def get_json_response(filename):
+    labels_dict = {}
+    response_dict = {}
+    try:
+        with open(filename, 'r') as labels:
+            labels_dict = json.load(labels)
+        response_dict[STATUS] = "true"
+        response_dict["labels_mapping"] = labels_dict
+        js_dump = json.dumps(response_dict)
+        resp = Response(js_dump,status=200,
+                        mimetype='application/json')
+    except FileNotFoundError as err:
+        response_dict = {'error': 'file not found in server'}
+        js_dump = json.dumps(response_dict)
+        resp = Response(js_dump,status=500,
+                        mimetype='application/json')
+    except RuntimeError as err:
+        response_dict = {'error': 'error occured on server side. Please try again'}
+        js_dump = json.dumps(response_dict)
+        resp = Response(js_dump, status=500,
+                        mimetype='application/json')
+    return resp
 
 if __name__ == '__main__':
-  APP.run(host='127.0.0.1', port=9000)
+    APP.run(host='127.0.0.1', port=5000)
--- backend/proper_noun_extractor.py
+++ backend/proper_noun_extractor.py
@@ -2,12 +2,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-def proper_noun_extractor(text):
+def proper_noun_extractor(text, noun_limit=2):
   out = ''
   words = nltk.word_tokenize(text)
   words = [word for word in words if word not in set(stopwords.words('english'))]
   tagged = nltk.pos_tag(words)
-  for (word, tag) in tagged[:3]:
+  for (word, tag) in tagged[:noun_limit + 1]:
     if tag == 'NNP':
       out += word + ' '
   return out.strip()
