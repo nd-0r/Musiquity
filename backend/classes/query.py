3ef9cc0165d1f5c55bfee93f6c511b949a099ab1
Andrew Orals <nd-0r@andreworals.com>
2021-01-17 03:29:53 -0600

    implemented query class

--- backend/classes/query.py
+++ backend/classes/query.py
@@ -1,12 +1,21 @@
-import re
-from parse_components import *
-
 class Query:
 
-  link = None
-  
-  artist = None
-  title = None
-  media_type = None
+  title = ''
+  artist = ''
+  media_type = ''
 
-  def __new__():
+  def __init__(self, title, artist='', media_type=''):
+    if not (isinstance(title, str)
+            and isinstance(artist, str)
+            and isinstance(media_type, str)):
+      raise TypeError(f'Cannot instantiate query object with arguments of types: \
+                        {type(title)}, {type(artist)}, {type(media_type)}')
+    self.title = title
+    self.artist = artist
+    self.media_type = media_type
+
+  def is_song(self):
+    return self.media_type == 'song'
+
+  def is_album(self):
+    return self.media_type == 'album'
