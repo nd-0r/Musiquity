d27703a6cb303d6b1c5b6f939da2ee4878dac161
Andrew Orals <nd-0r@andreworals.com>
2021-01-17 19:12:04 -0600

    enabled cors on app

--- heroku-code/backend/app.py
+++ heroku-code/backend/app.py
@@ -1,8 +1,10 @@
 from backend.search import search
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 import json
 APP = Flask(__name__)
+CORS(APP)
 API = Api(APP)
 
 parser = reqparse.RequestParser()
